9c6767f7ff7afaaa814459d75217d8d679250803
yawara <ni732h.sphl@gmail.com>
2015-08-05 10:16:09 +0900

    m

--- gbc_boolean.py
+++ gbc_boolean.py
@@ -50,7 +50,7 @@ def gbc_boolean(B):
   
   for i, l1 in enumerate(lines):
     for j, l2 in enumerate(lines):
-      if i <= j:
+      if i < j:
         if l1[0]*l2[0]+l1[1]*l2[1]+l1[2]*l2[2] == 0:
           G.add_edge(l1, l2)
 
